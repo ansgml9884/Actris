d604277519b620edcc8b52e0779ef03cc8e8f67a
kimmunhee <ansgml9884@naver.com>
2020-09-03 11:11:28 +0900

    edit camera script

--- front-end/demos/camera.js
+++ front-end/demos/camera.js
@@ -15,19 +15,15 @@
  * =============================================================================
  */
 import * as posenet from '@tensorflow-models/posenet';
-import dat from 'dat.gui';
 import Stats from 'stats.js';
 
 import {startGame} from './tetris.js'
-import {drawBoundingBox, drawKeypoints, drawSkeleton, isMobile, toggleLoadingUI, tryResNetButtonName, tryResNetButtonText, updateTryResNetButtonDatGuiCss} from './demo_util';
+import {drawBoundingBox, drawKeypoints, drawSkeleton, isMobile, toggleLoadingUI} from './demo_util';
 
 const videoWidth = 600;
 const videoHeight = 500;
 const stats = new Stats();
-/**
- * Loads a the camera to be used in the demo
- *
- */
+
 async function setupCamera() {
   if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
     throw new Error(
@@ -64,17 +60,34 @@ async function loadVideo() {
 }
 
 const defaultQuantBytes = 2;
-
 const defaultMobileNetMultiplier = isMobile() ? 0.50 : 0.75;
 const defaultMobileNetStride = 16;
 const defaultMobileNetInputResolution = 500;
+  // Input resolution:  Internally, this parameter affects the height and width
+  // of the layers in the neural network. The higher the value of the input
+  // resolution the better the accuracy but slower the speed.
 
-const defaultResNetMultiplier = 1.0;
-const defaultResNetStride = 32;
-const defaultResNetInputResolution = 250;
+  // Output stride:  Internally, this parameter affects the height and width of
+  // the layers in the neural network. The lower the value of the output stride
+  // the higher the accuracy but slower the speed, the higher the value the
+  // faster the speed but lower the accuracy.
+
+  // Multiplier: this parameter affects the number of feature map channels in
+  // the MobileNet. The higher the value, the higher the accuracy but slower the
+  // speed, the lower the value the faster the speed but lower the accuracy.
+
+  // QuantBytes: this parameter affects weight quantization in the ResNet50
+  // model. The available options are 1 byte, 2 bytes, and 4 bytes. The higher
+  // the value, the larger the model size and thus the longer the loading time,
+  // the lower the value, the shorter the loading time but lower the accuracy.
+  
+  // Pose confidence: the overall confidence in the estimation of a person's
+  // pose (i.e. a person detected in a frame)
+  // Min part confidence: the confidence that a particular estimated keypoint
+  // position is accurate (i.e. the elbow's position)
 
 const guiState = {
-  algorithm: 'multi-pose',
+  algorithm: 'single-pose',
   input: {
     architecture: 'MobileNetV1',
     outputStride: defaultMobileNetStride,
@@ -85,12 +98,6 @@ const guiState = {
   singlePoseDetection: {
     minPoseConfidence: 0.1,
     minPartConfidence: 0.5,
-  },
-  multiPoseDetection: {
-    maxPoseDetections: 5,
-    minPoseConfidence: 0.15,
-    minPartConfidence: 0.1,
-    nmsRadius: 30.0,
   },
   output: {
     showVideo: true,
@@ -111,157 +118,7 @@ function setupGui(cameras, net) {
     guiState.camera = cameras[0].deviceId;
   }
 
-  const gui = new dat.GUI({width: 300});
-  dat.GUI.toggleHide();
-
-  let architectureController = null;
-  guiState[tryResNetButtonName] = function() {
-    architectureController.setValue('ResNet50')
-  };
-  gui.add(guiState, tryResNetButtonName).name(tryResNetButtonText);
-  updateTryResNetButtonDatGuiCss();
-
-  // The single-pose algorithm is faster and simpler but requires only one
-  // person to be in the frame or results will be innaccurate. Multi-pose works
-  // for more than 1 person
-  const algorithmController =
-      gui.add(guiState, 'algorithm', ['single-pose', 'multi-pose']);
-
-  // The input parameters have the most effect on accuracy and speed of the
-  // network
-  let input = gui.addFolder('Input');
-  // Architecture: there are a few PoseNet models varying in size and
-  // accuracy. 1.01 is the largest, but will be the slowest. 0.50 is the
-  // fastest, but least accurate.
-  architectureController =
-      input.add(guiState.input, 'architecture', ['MobileNetV1', 'ResNet50']);
   guiState.architecture = guiState.input.architecture;
-  // Input resolution:  Internally, this parameter affects the height and width
-  // of the layers in the neural network. The higher the value of the input
-  // resolution the better the accuracy but slower the speed.
-  let inputResolutionController = null;
-  function updateGuiInputResolution(
-      inputResolution,
-      inputResolutionArray,
-  ) {
-    if (inputResolutionController) {
-      inputResolutionController.remove();
-    }
-    guiState.inputResolution = inputResolution;
-    guiState.input.inputResolution = inputResolution;
-    inputResolutionController =
-        input.add(guiState.input, 'inputResolution', inputResolutionArray);
-    // inputResolutionController.onChange(function(inputResolution) {
-    //   guiState.changeToInputResolution = inputResolution;
-    // });
-  }
-
-  // Output stride:  Internally, this parameter affects the height and width of
-  // the layers in the neural network. The lower the value of the output stride
-  // the higher the accuracy but slower the speed, the higher the value the
-  // faster the speed but lower the accuracy.
-  let outputStrideController = null;
-  function updateGuiOutputStride(outputStride, outputStrideArray) {
-    if (outputStrideController) {
-      outputStrideController.remove();
-    }
-    guiState.outputStride = outputStride;
-    guiState.input.outputStride = outputStride;
-    outputStrideController =
-        input.add(guiState.input, 'outputStride', outputStrideArray);
-    // outputStrideController.onChange(function(outputStride) {
-    //   guiState.changeToOutputStride = outputStride;
-    // });
-  }
-
-  // Multiplier: this parameter affects the number of feature map channels in
-  // the MobileNet. The higher the value, the higher the accuracy but slower the
-  // speed, the lower the value the faster the speed but lower the accuracy.
-  let multiplierController = null;
-  function updateGuiMultiplier(multiplier, multiplierArray) {
-    if (multiplierController) {
-      multiplierController.remove();
-    }
-    guiState.multiplier = multiplier;
-    guiState.input.multiplier = multiplier;
-    multiplierController =
-        input.add(guiState.input, 'multiplier', multiplierArray);
-    // multiplierController.onChange(function(multiplier) {
-    //   guiState.changeToMultiplier = multiplier;
-    // });
-  }
-
-  // QuantBytes: this parameter affects weight quantization in the ResNet50
-  // model. The available options are 1 byte, 2 bytes, and 4 bytes. The higher
-  // the value, the larger the model size and thus the longer the loading time,
-  // the lower the value, the shorter the loading time but lower the accuracy.
-  let quantBytesController = null;
-  function updateGuiQuantBytes(quantBytes, quantBytesArray) {
-    if (quantBytesController) {
-      quantBytesController.remove();
-    }
-    guiState.quantBytes = +quantBytes;
-    guiState.input.quantBytes = +quantBytes;
-    quantBytesController =
-        input.add(guiState.input, 'quantBytes', quantBytesArray);
-    // quantBytesController.onChange(function(quantBytes) {
-    //   guiState.changeToQuantBytes = +quantBytes;
-    // });
-  }
-
-  function updateGui() {
-    if (guiState.input.architecture === 'MobileNetV1') {
-      updateGuiInputResolution(
-          defaultMobileNetInputResolution, 400);
-          //[200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750, 800]);
-      updateGuiOutputStride(defaultMobileNetStride, [8, 16]);
-      updateGuiMultiplier(defaultMobileNetMultiplier, [0.50, 0.75, 1.0]);
-    } else {  // guiState.input.architecture === "ResNet50"
-      updateGuiInputResolution(
-          defaultResNetInputResolution,
-          [200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750, 800]);
-      updateGuiOutputStride(defaultResNetStride, [32, 16]);
-      updateGuiMultiplier(defaultResNetMultiplier, [1.0]);
-    }
-    updateGuiQuantBytes(defaultQuantBytes, [1, 2, 4]);
-  }
-
-  updateGui();
-  input.open();
-  // Pose confidence: the overall confidence in the estimation of a person's
-  // pose (i.e. a person detected in a frame)
-  // Min part confidence: the confidence that a particular estimated keypoint
-  // position is accurate (i.e. the elbow's position)
-  let single = gui.addFolder('Single Pose Detection');
-  single.add(guiState.singlePoseDetection, 'minPoseConfidence', 0.0, 1.0);
-  single.add(guiState.singlePoseDetection, 'minPartConfidence', 0.0, 1.0);
-
-  let output = gui.addFolder('Output');
-  output.add(guiState.output, 'showVideo');
-  output.add(guiState.output, 'showSkeleton');
-  output.add(guiState.output, 'showPoints');
-  output.add(guiState.output, 'showBoundingBox');
-  output.open();
-
-
-  // architectureController.onChange(function(architecture) {
-  //   // if architecture is ResNet50, then show ResNet50 options
-  //   updateGui();
-  //   guiState.changeToArchitecture = architecture;
-  // });
-
-  // algorithmController.onChange(function(value) {
-  //   switch (guiState.algorithm) {
-  //     case 'single-pose':
-  //       multi.close();
-  //       single.open();
-  //       break;
-  //     case 'multi-pose':
-  //       single.close();
-  //       multi.open();
-  //       break;
-  //   }
-  // });
 }
 
 /**
@@ -384,19 +241,6 @@ function detectPoseInRealTime(video, net) {
         minPoseConfidence = +guiState.singlePoseDetection.minPoseConfidence;
         minPartConfidence = +guiState.singlePoseDetection.minPartConfidence;
         break;
-      case 'multi-pose':
-        let all_poses = await guiState.net.estimatePoses(video, {
-          flipHorizontal: flipPoseHorizontal,
-          decodingMethod: 'multi-person',
-          maxDetections: guiState.multiPoseDetection.maxPoseDetections,
-          scoreThreshold: guiState.multiPoseDetection.minPartConfidence,
-          nmsRadius: guiState.multiPoseDetection.nmsRadius
-        });
-
-        poses = poses.concat(all_poses);
-        minPoseConfidence = +guiState.multiPoseDetection.minPoseConfidence;
-        minPartConfidence = +guiState.multiPoseDetection.minPartConfidence;
-        break;
     }
 
     ctx.clearRect(0, 0, videoWidth, videoHeight);
@@ -428,10 +272,8 @@ function detectPoseInRealTime(video, net) {
 
     // End monitoring code for frames per second
     stats.end();
-
     requestAnimationFrame(poseDetectionFrame);
   }
-
   poseDetectionFrame();
 }
 
@@ -468,7 +310,7 @@ export async function bindPage() {
 }
 navigator.getUserMedia = navigator.getUserMedia ||
     navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-// kick off the demo
+
 startGame();
 bindPage();
 
